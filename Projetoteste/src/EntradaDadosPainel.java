d48871fc81354c30a2dd3a48c81aa07343602f14
Carlos <Carlos@192.168.0.7>
2021-08-30 07:41:11 -0300

    autualizando a entrada de informações com o JOptionPane

--- Projetoteste/src/EntradaDadosPainel.java
+++ Projetoteste/src/EntradaDadosPainel.java
@@ -15,6 +15,7 @@ public class EntradaDadosPainel {
 				"Nome informado " + nome + " idade informada " + idade + " Quantidade de carros informada: " + carros);
 
 		PrimeiraClasseJava();
+		SegundaClasseJava();
 	}
 
 	public static void vento() {
@@ -44,4 +45,34 @@ public class EntradaDadosPainel {
 			JOptionPane.showMessageDialog(null, "O resto da divisão foi: " + resto);
 	}
 }
+	
+	public static void SegundaClasseJava() {
+		
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1: ");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2: ");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3: ");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4: ");
+		
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+		
+		double media = (dNota1+dNota2+dNota3+dNota4) / 4;
+		
+		if (media >= 50) {
+			if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com media de: " + media);
+			}else {
+				JOptionPane.showMessageDialog(null, "Aluno em recuperação com media de: " + media);
+				
+			}
+				
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Aluno está reprovado com média de: " + media);
+		}
+		JOptionPane.showMessageDialog(null, "A media é: " +  media);
+		
+	}
 }
